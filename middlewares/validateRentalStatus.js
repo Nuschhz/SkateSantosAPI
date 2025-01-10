cb7003539d2168e8aaa0a9ee67a982d3af659e19
Nuschhz <nusch05@hotmail.com>
2025-01-10 00:07:22 -0300

    feat(middlewares): Added rentalStatus middleware validation

---
+++ middlewares/validateRentalStatus.js
@@ -0,0 +1,23 @@
+const validateRentalStatus = (req, res, next) => {
+  const { status } = req.body;
+  const validStatuses = ["canceled", "done"]; // Valores válidos para o status
+
+  // Verifica se o status é fornecido e do tipo "number"
+  if (
+    typeof status !== "number" ||
+    status < 0 ||
+    status >= validStatuses.length
+  ) {
+    return res.status(400).json({
+      message:
+        "O status deve ser um número válido (0 para 'canceled', 1 para 'done').",
+    });
+  }
+
+  // Adiciona o status mapeado no objeto da requisição para uso no controlador
+  req.body.statusMapped = validStatuses[status];
+
+  next(); // Passa para o próximo middleware ou controlador
+};
+
+module.exports = validateRentalStatus;
