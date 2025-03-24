9b7b6ded3bd4baaa090dfa2079065ee638620ee1
Nuschhz <nusch05@hotmail.com>
2025-03-24 16:55:12 -0300

    fix(ip): Corrected ip problems

--- app.js
+++ app.js
@@ -52,4 +52,4 @@ app.use("/api/stations", stationRoutes);
 app.use("/api/tickets", ticketRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Servidor rodando na porta ${PORT}`));
